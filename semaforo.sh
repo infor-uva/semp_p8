24f3722d1767d9c0024b217f79b833cac28ee87d
migudel <migudel@outlook.com>
2025-05-16 10:10:23 +0200

    rainbow

--- semaforo.sh
+++ semaforo.sh
@@ -23,7 +23,9 @@ outputs="$ctr $cta $ctv $ptr $ptv"
 lock_prefix=/tmp/semaforo.
 lock_extension=.lock
 magic_lock_file=${lock_prefix}magic$lock_extension
-press_lock_file=${lock_prefix}press_info$lock_extension
+
+# flags
+yellow_blink=1
 
 ##################################################
 ############    Helpers functions    #############
@@ -42,8 +44,11 @@ range () {
 }
 
 all() {
+  command="$1"
+  shift
 	for i in $outputs;do
-		$1 $i &
+    $@
+		$command $i &
 	done
 }
 
@@ -54,10 +59,7 @@ all() {
 setup() {
 	echo Configure and testing interfaces...
 	# Configure outputs
-	for i in $outputs; do
-		gpio mode $i out
-		on $i &
-	done
+  all on gpio mode $i out
 	# Configure motor
 	gpio mode $mot pwm
 	range $mot 50 
@@ -84,12 +86,10 @@ action () {
 	# After a half second (pedestrians start walking)
 	range $mot 50 & sleep 3.8
 	# d. After four seconds (the tls -car's yellow and pedestrian's green- start to blink)
-	off $ctr & on $cta
-	# And pedestrians start to running
-	range $mot 100 & sleep .1
+	off $ctr & { [ $yellow_blink = 0 ] && on $cta } & range $mot 100 & sleep .1
 	for _ in $(seq 3); do
-		off $cta & off $ptv & sleep .5
-		on $cta & on $ptv & sleep .5
+		{ [ $yellow_blink = 0 ] && off $cta } & off $ptv & sleep .5
+		{ [ $yellow_blink = 0 ] && on $cta } & on $ptv & sleep .5
 	done
 	# After 7 seconds (pedestrian tl change from green to red)
 	# Pedestrians stop running
@@ -117,7 +117,25 @@ test_mode () {
 	sleep .5
 }
 
-shutdown() {
+rainbow() {
+  all off
+  all on sleep 0.5
+  all off sleep 0.5
+  f=0.7
+  f2=.5
+  while true; do
+    for i in $outputs; do
+      { on $i && sleep $f && off $i } &
+      sleep $f2
+    done
+  done
+}
+
+alter_yellow_blink_mode() {
+  rainbow
+}
+
+safe_shutdown() {
 	echo Shutting down devices
 	all off
 	range $mot 0
@@ -131,7 +149,7 @@ shutdown() {
 ##################################################
 
 ### Safe to shutdown devices before unexpected stop (SIGINT or Ctrl + c)
-trap 'shutdown' SIGINT
+trap 'safe_shutdown' SIGINT
 
 
 ##################################################
@@ -145,19 +163,36 @@ while true; do
 	while [ `gpio read $but` = 1 ]; do sleep .1; done
 	echo "Button pressed"
 	
-	touch $press_lock_file
-	press_info & start=$(date +%s)
-	while [ `gpio read $but` = 0 ]; do sleep .1; done
+	mode=0
+	start=$(date +%s)
+	while [ `gpio read $but` = 0 ]; do 
+		sleep .1
+		end=$(date +%s)
+		time=$(( $end - $start ))
+		if [ $mode = 0 ] && [ $time -ge 2 ]; then 
+			echo TEST MODE ACTIVATE
+			mode=1
+		elif [ $mode = 1 ] && [ $time -ge 4 ]; then 
+			echo RAINBOW MODE ACTIVATE
+			mode=2
+		fi
+	done
 	end=$(date +%s)
 	time=$(( $end - $start ))
-	rm $press_lock_file
 	echo "Button released ($time seg)"
 
-	if [ $time -le 2 ]; then
-		action
-	elif [ $time -le 4 ]; then
-		test_mode
-	else
-		echo "RAINBOW"
-	fi
+	case $mode in 
+		0)
+			action 
+			;;
+		1)
+			test_mode
+			;;
+    2)
+      alter_yellow_blink_mode
+      ;;
+		*)
+			echo RAINBOW
+			;;
+	esac
 done
